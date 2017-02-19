daca2fc8be74b139610f17c3c8e4cdb167709a4c
ictxiangxin <ictxiangxin@gmail.com>
2017-02-19 14:17:50 +0800

    Initial commit

---
+++ logistic_regression.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import numpy as np
+
+
+class LogisticRegression:
+    def __init__(self, rate: float, loss_threshold: float=0.0001, max_epochs: int=1000):
+        self.__rate = rate
+        self.__loss_threshold = tf.constant(loss_threshold)
+        self.__max_epochs = max_epochs
+        self.__session = tf.Session()
+        self.__w = None
+        self.__b = None
+
+    def w(self):
+        if self.__w is not None:
+            return self.__session.run(self.__w)
+
+    def b(self):
+        if self.__b is not None:
+            return self.__session.run(self.__b)
+
+    def train(self, train_data, target):
+        x_data = np.array(train_data)
+        y_data = np.array(target)
+        _x = tf.placeholder(tf.float32)
+        _y = tf.placeholder(tf.float32)
+        if len(x_data.shape) == 1:
+            self.__w = tf.Variable(tf.zeros([1]))
+        else:
+            self.__w = tf.Variable(tf.zeros([x_data.shape[1], 1]))
+        if len(y_data.shape) == 1:
+            self.__b = tf.Variable(tf.zeros([1]))
+        else:
+            self.__b = tf.Variable(tf.zeros([y_data.shape, 1]))
+        _f = tf.nn.softmax(tf.multiply(_x, self.__w) + self.__b)
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(_f - _y))
+        optimizer = tf.train.GradientDescentOptimizer(self.__rate)
+        model = optimizer.minimize(loss)
+        self.__session.run(tf.global_variables_initializer())
+        for epoch in range(self.__max_epochs):
+            self.__session.run(model, {_x: x_data, _y: y_data})
+            if epoch % 10 == 0:
+                if self.__session.run(loss < self.__loss_threshold, {_x: x_data, _y: y_data}):
+                    break
+
+    def predict(self, predict_data):
+        x_data = np.array(predict_data)
+        _x = tf.placeholder(tf.float32)
+        return self.__session.run(tf.multiply(_x, self.__w) + self.__b, {_x: x_data})
